f2d4c2def6f9b3462f9f6337e301656ae82cb02c
William Benton <self@willbenton.com>
2016-02-02 21:32:05 -0600

    Use silex for natural-language features

---
+++ analysis/src/main/scala/com/redhat/et/descry/text/messageFeatures.scala
@@ -0,0 +1,30 @@
+/*
+ * messageFeatures.scala
+ * 
+ * author:  William Benton <willb@redhat.com>
+ *
+ * Copyright (c) 2016 Red Hat, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import com.redhat.et.silex.text._
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{DataFrame, Row}
+import org.apache.spark.mllib.feature.{Word2Vec, Word2VecModel}
+
+object MessageFeaturizer {
+  def messages(df: DataFrame, col: String): RDD[String] =  df.select(col).rdd map { case Row(s: String) => s }
+  def model(msgRDD: RDD[String]): Word2VecModel = { (new Word2Vec).fit( msgRDD.map { msg => LogTokenizer.tokens(msg) } ) }
+}
