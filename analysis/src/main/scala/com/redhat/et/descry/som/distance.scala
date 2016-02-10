4aaad39c34421f3d5ab48abfd08094b613d1acc4
William Benton <self@willbenton.com>
2016-02-10 09:45:40 -0600

    alternate implementation

--- analysis/src/main/scala/com/redhat/et/descry/som/distance.scala
+++ analysis/src/main/scala/com/redhat/et/descry/som/distance.scala
@@ -21,9 +21,10 @@
 package com.redhat.et.descry.som;
 
 import org.apache.spark.mllib.linalg.{Vector=>V, DenseVector=>DV, SparseVector=>SV}
+import scala.annotation.tailrec
 
 class AngularSimilarity extends Function2[V, V, Double] {
-  @inline protected def sparsify: V => SV = {
+  @inline def sparsify: V => SV = {
     case d@DV(_) => d.toSparse
     case s@SV(_, _, _) => s
   } 
@@ -33,23 +34,27 @@ class AngularSimilarity extends Function2[V, V, Double] {
   }
   
   @inline protected def dot(v1: SV, v2: SV): Double = {
-    ((v1 match { case SV(_, is, _) => is.toSet })
-      .intersect(v2 match { case SV(_, is, _) => is.toSet }))
-      .toArray
-      .map { i => v1(i) * v2(i) }
-      .sum
+    val SV(_, i1s, v1s) = v1
+    val SV(_, i2s, v2s) = v2
+    dotImpl((i1s zip v1s).toList, (i2s zip v2s).toList)
+  }
+  
+  @tailrec final private def dotImpl(v1: List[Pair[Int, Double]], v2: List[Pair[Int, Double]], acc:Double = 0d): Double = (v1, v2) match {
+    case ((i1,_)::e1s, (i2,_)::e2s) if i1 < i2 => dotImpl(e1s, v2, acc)
+    case ((i1,_)::e1s, (i2,_)::e2s) if i1 > i2 => dotImpl(v1, e2s, acc)
+    case ((i1,v1)::e1s, (i2,v2)::e2s) if i1 == i2 => dotImpl(e1s, e2s, acc + (v1 * v2))
+    case (Nil, _) => acc
+    case (_, Nil) => acc
   }
   
   @inline protected def cossim(v1: SV, v2: SV): Double = {
-    val num = dot(sparsify(v1), sparsify(v2))
-    val den = magnitude(sparsify(v1)) * magnitude(sparsify(v2))
-    num / den
+    val n = dot(v1, v2)
+    val d = magnitude(v1) * magnitude(v2)
+    math.min(1.0, math.max(-1.0, n / d))
   }
   
   def apply(v1: V, v2: V): Double = {
-    val num = dot(sparsify(v1), sparsify(v2))
-    val den = magnitude(sparsify(v1)) * magnitude(sparsify(v2))
-    1 - (2 * math.acos(num / den) / math.Pi)
+    1 - (2 * math.acos(cossim(sparsify(v1), sparsify(v2))) / math.Pi)
   }
 }
 
