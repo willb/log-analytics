d4b4b33cec54c3e88c75024f8d64a0314469b754
William Benton <self@willbenton.com>
2016-03-31 13:19:49 -0500

    add example count to SOM example

--- analysis/src/main/scala/com/redhat/et/descry/som/somexample.scala
+++ analysis/src/main/scala/com/redhat/et/descry/som/somexample.scala
@@ -6,16 +6,16 @@ object Example {
   import com.redhat.et.descry.util.ImageWriter
   import org.apache.spark.SparkContext
   
-  def apply(xdim: Int, ydim: Int, iterations: Int, sc: SparkContext): SOM = {
+  def apply(xdim: Int, ydim: Int, iterations: Int, sc: SparkContext, exampleCount: Int): SOM = {
     val rnd = new scala.util.Random()
-    val colors = Array.fill(200000)(new DV(Array.fill(3)(rnd.nextDouble)).compressed)
+    val colors = Array.fill(exampleCount)(new DV(Array.fill(3)(rnd.nextDouble)).compressed)
     val examples = sc.parallelize(colors)
     
     def writeStep(step: Int, som: SOM) {
       ImageWriter.write(xdim, ydim, som.entries, "som-step-%04d.png".format(step))
     }
     
-    com.redhat.et.descry.som.SOM.train(xdim, ydim, 3, iterations, examples, hook=writeStep _)
+    com.redhat.et.descry.som.SOM.train(xdim, ydim, 3, iterations, examples, sigmaScale=0.7, hook=writeStep _)
   }
   
 }
