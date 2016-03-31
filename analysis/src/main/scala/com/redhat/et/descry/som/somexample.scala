a6c2e35835f3408e265f435a8c7bf48ffe7e144a
William Benton <self@willbenton.com>
2016-03-31 09:47:08 -0500

    added self-contained SOM example

---
+++ analysis/src/main/scala/com/redhat/et/descry/som/somexample.scala
@@ -0,0 +1,21 @@
+package com.redhat.et.descry.som
+
+object Example {
+  import org.apache.spark.mllib.linalg.{DenseVector => DV}
+  import com.redhat.et.descry.som.SOM
+  import com.redhat.et.descry.util.ImageWriter
+  import org.apache.spark.SparkContext
+  
+  def apply(xdim: Int, ydim: Int, iterations: Int, sc: SparkContext): SOM = {
+    val rnd = new scala.util.Random()
+    val colors = Array.fill(200000)(new DV(Array.fill(3)(rnd.nextDouble)).compressed)
+    val examples = sc.parallelize(colors)
+    
+    def writeStep(step: Int, som: SOM) {
+      ImageWriter.write(xdim, ydim, som.entries, "som-step-%04d.png".format(step))
+    }
+    
+    com.redhat.et.descry.som.SOM.train(xdim, ydim, 3, iterations, examples, hook=writeStep _)
+  }
+  
+}
