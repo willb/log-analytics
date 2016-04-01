8a86fa8c610794bb5708e6b3f0d602c2e46bdab5
William Benton <self@willbenton.com>
2016-03-31 21:34:51 -0500

    added som.Example.profile()

--- analysis/src/main/scala/com/redhat/et/descry/som/somexample.scala
+++ analysis/src/main/scala/com/redhat/et/descry/som/somexample.scala
@@ -18,4 +18,14 @@ object Example {
     com.redhat.et.descry.som.SOM.train(xdim, ydim, 3, iterations, examples, sigmaScale=0.7, hook=writeStep _)
   }
   
+  def profile(sc: SparkContext, xdim: Int = 256, ydim: Int = 144, fdim: Int = 8, partitions: Int = 128, iterations: Int = 40, exampleCount: Int = 200000): SOM = {
+    val rnd = new scala.util.Random()
+    val features = math.max(fdim, 3)
+    val colors = Array.fill(exampleCount)(new DV(Array.fill(features)(rnd.nextDouble)).compressed)
+    val examples = sc.parallelize(colors).repartition(partitions)
+  
+    def writeStep(step: Int, som: SOM) { }
+
+    com.redhat.et.descry.som.SOM.train(xdim, ydim, features, iterations, examples, sigmaScale=0.7, hook=writeStep _)
+  }
 }
